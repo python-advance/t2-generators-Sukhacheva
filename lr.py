6c283f9d0692ae06342c07636b42db4c650d87e1
Сухачева Валерия <31817397+Sukhacheva@users.noreply.github.com>
2020-06-27 00:40:08 +0300

    Update lr.py

--- lr.py
+++ lr.py
@@ -8,13 +8,23 @@ def get_fib_nums_lst(n):
 	n - количество чисел в списке 
 
 	"""
+  lst = [0, 1, 1]
+  fib1, fib2 = 1, 1
   if n == 0:
-      print(0)
+    quit()
+  if n == 1:
+    print(lst[0])
+  if n == 2:
+    print(lst[0:2])
   else:
-      a, b = 0, 1
-      for i in range(2, n + 1):
-          a, b = b, a + b
-      print(b)
+    for i in range(3, n):
+      fib1, fib2 = fib2, fib1 + fib2
+      lst.append(fib2)
+  print(lst)
+  return lst
+
+n = int(input('Введите количество чисел: '))
+get_fib_nums_lst(n)
 
 
 
