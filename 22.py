55ad3f363b33d2dc9369f3416996f8f64b687445
Сухачева Валерия <31817397+Sukhacheva@users.noreply.github.com>
2020-06-27 00:46:26 +0300

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,12 @@
+# Python3 код для генерации
+# id с помощью uuid4 ()
+
+import uuid
+
+id = uuid.uuid4()
+  
+# Идентификатор сгенерированный с помощью uuid4 ()
+
+print ("The id generated using uuid4() : ",end="")
+
+print (id)
